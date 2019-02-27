08675449c2f63afcbff80e28a72996a0a10dc014
jusax23 <jusax23@gmail.com>
2019-02-27 20:13:30 +0100

    widget

--- app/src/main/java/com/cloudway/mcpieper/Widget_one_Provider.java
+++ app/src/main/java/com/cloudway/mcpieper/Widget_one_Provider.java
@@ -17,57 +17,72 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Date;
 
 public class Widget_one_Provider extends AppWidgetProvider {
     @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        for (int appWidgetId : appWidgetIds) {
+    public void onUpdate(Context context, final AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (final int appWidgetId : appWidgetIds) {
 
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_one);
+            final RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_one_style);
             /*views.setTextViewText(R.id.widget_one_content, "Kein Internet");
             views.setTextViewText(R.id.widget_one_titel, "Sanitäter(heute):");*/
             SharedPreferences sharedPreferences = context.getSharedPreferences("login", 0);
 
-            if (sharedPreferences.getBoolean("authed", false) && false) {
+            if (sharedPreferences.getBoolean("authed", false)) {
                 final String usr = sharedPreferences.getString("usr", "");
                 final String pwd = sharedPreferences.getString("pwd", "");
-                try {
-                    URL url = new URL("http://jusax.dnshome.de/s/" + "sani.php?d=&act=a&un=" + usr + "&key=" + pwd);
-                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                    try {
-                        InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                        BufferedReader r = new BufferedReader(new InputStreamReader(in));
-                        StringBuilder total = new StringBuilder();
-                        String line;
-                        while ((line = r.readLine()) != null) {
-                            total.append(line);
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            URL url = new URL("http://jusax.dnshome.de/s/" + "sani.php?d=&act=a&un=" + usr + "&key=" + pwd);
+                            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                            try {
+                                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                                BufferedReader r = new BufferedReader(new InputStreamReader(in));
+                                StringBuilder total = new StringBuilder();
+                                String line;
+                                while ((line = r.readLine()) != null) {
+                                    total.append(line);
+                                }
+                                final String[] result = total.toString().split(";");
+                                if (result[1].contains("m")) {
+                                    views.setTextViewText(R.id.widget_one_titel, "Sanitäter\n(morgen):");
+                                } else {
+                                    views.setTextViewText(R.id.widget_one_titel, "Sanitäter\n(heute):");
+                                }
+                                String group = "";
+                                if (result.length != 3) {
+                                    views.setTextViewText(R.id.widget_one_content, "keiner");
+                                } else {
+                                    views.setTextViewText(R.id.widget_one_content, result[2]);
+                                }
+                                Date d = new Date();
+                                views.setTextViewText(R.id.widget_one_offline, "Zuletzt aktualisiert: "+String.valueOf(d.getHours())+":"+String.valueOf(d.getMinutes()));
+                            } finally {
+                                appWidgetManager.updateAppWidget(appWidgetId, views);
+                                urlConnection.disconnect();
+
+                            }
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                            views.setTextViewText(R.id.widget_one_content, "Kein Internet");
+                            views.setTextViewText(R.id.widget_one_titel, "Sanitäter\n(?):");
+                            appWidgetManager.updateAppWidget(appWidgetId, views);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            views.setTextViewText(R.id.widget_one_content, "Kein Internet");
+                            views.setTextViewText(R.id.widget_one_titel, "Sanitäter\n(?):");
+                            Date d = new Date();
+                            views.setTextViewText(R.id.widget_one_offline, "(Offline) "+String.valueOf(d.getHours())+String.valueOf(d.getMinutes()));
+                            appWidgetManager.updateAppWidget(appWidgetId, views);
                         }
-                        final String[] result = total.toString().split(";");
-                        if (result[1].contains("m")) {
-                            views.setTextViewText(R.id.widget_one_titel, "Sanitäter(morgen):");
-                        } else {
-                            views.setTextViewText(R.id.widget_one_titel, "Sanitäter(heute):");
-                        }
-                        String group = "";
-                        if (result.length != 3) {
-                            views.setTextViewText(R.id.widget_one_content, "keiner");
-                        } else {
-                            views.setTextViewText(R.id.widget_one_content, group);
-                        }
-                    } finally {
-                        urlConnection.disconnect();
-                        views.setTextViewText(R.id.widget_one_content, "Kein Internet");
-                        views.setTextViewText(R.id.widget_one_titel, "Sanitäter(?):");
+
                     }
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                    views.setTextViewText(R.id.widget_one_content, "Kein Internet");
-                    views.setTextViewText(R.id.widget_one_titel, "Sanitäter(?):");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    views.setTextViewText(R.id.widget_one_content, "Kein Internet");
-                    views.setTextViewText(R.id.widget_one_titel, "Sanitäter(?):");
-                }
+                });
+                thread.start();
+
             }else{
                 Intent intent = new Intent(context, MainActivity.class);
                 PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
@@ -76,7 +91,6 @@ public class Widget_one_Provider extends AppWidgetProvider {
                 views.setTextViewText(R.id.widget_one_titel, "Bitte anmelden.");
                 views.setOnClickPendingIntent(R.id.widget_one, pendingIntent);
             }
-            Toast.makeText(context,"aktualiesiere", Toast.LENGTH_SHORT).show();
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
