9c60fe238f4db00bd59b154651227b12be4ffbbe
jusax23 <jusax23@gmail.com>
2019-03-03 18:11:47 +0100

    widget/keeper

--- app/src/main/java/com/cloudway/mcpieper/autostart.java
+++ app/src/main/java/com/cloudway/mcpieper/autostart.java
@@ -1,5 +1,7 @@
 package com.cloudway.mcpieper;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -13,6 +15,11 @@ public class autostart extends BroadcastReceiver {
             Intent i = new Intent(context, NotificationMgr.class);
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startService(i);
+
+            AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            Intent intent_alarm = new Intent(context, Receiver.class);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 1, intent_alarm, 0);
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 10000, pendingIntent);
         }
     }
 }
