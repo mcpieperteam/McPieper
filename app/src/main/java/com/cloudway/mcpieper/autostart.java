70357c79738e7d26e95de76ce53bcee43c12be20
jusax23 <jusax23@gmail.com>
2019-03-03 20:41:26 +0100

    widget/keeper

--- app/src/main/java/com/cloudway/mcpieper/autostart.java
+++ app/src/main/java/com/cloudway/mcpieper/autostart.java
@@ -1,27 +1,44 @@
 package com.cloudway.mcpieper;
 
 import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
+import java.util.Date;
+
 public class autostart extends BroadcastReceiver {
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            Intent i = new Intent(context, NotificationMgr.class);
-            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startService(i);
-
-            AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-            Intent intent_alarm = new Intent(context, Receiver.class);
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 1, intent_alarm, 0);
-            alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 10000, pendingIntent);
+            try {
+                AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+                Intent intent_alarm = new Intent(context, Receiver.class);
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 1, intent_alarm, 0);
+                alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+10, pendingIntent);
+                try{
+                    Intent i = new Intent(context, NotificationMgr.class);
+                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startService(i);
+                    Toast.makeText(context, "Autostart", Toast.LENGTH_LONG).show();
+                }catch (Exception e){
+                    Toast.makeText(context, "McPieper halb gestartet!!!\n"+e.toString(), Toast.LENGTH_LONG).show();
+                }
+            }catch (Exception e){
+                Toast.makeText(context, "Bitte starte Die McPieper app!!!", Toast.LENGTH_LONG).show();
+            }
         }
     }
+
 }
 
 
