126e18ded84813a7e0073f4b3d815f79cdb06451
jusax23 <jusax23@gmail.com>
2019-03-03 20:41:26 +0100

    widget/keeper

--- app/src/main/java/com/cloudway/mcpieper/Receiver.java
+++ app/src/main/java/com/cloudway/mcpieper/Receiver.java
@@ -2,22 +2,36 @@ package com.cloudway.mcpieper;
 
 import android.app.ActivityManager;
 import android.app.AlarmManager;
+import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
 
 public class Receiver extends BroadcastReceiver {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onReceive(Context context, Intent intent_old) {
+        Toast.makeText(context, "Receiver", Toast.LENGTH_LONG).show();
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(context, Receiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 1, intent, 0);
-        alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + /*1800000*/100000, pendingIntent);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 1800000, pendingIntent);
 
-        if(isServiceRunning(NotificationMgr.class, context)){
-            context.startService(new Intent(context, NotificationMgr.class));
+        if(!isServiceRunning(NotificationMgr.class, context)){
+            try{
+                context.startService(new Intent(context, NotificationMgr.class));
+            }catch (Exception e){
+                showNotification("Error", e.toString(),context);
+            }
+
         }
     }
     private boolean isServiceRunning (Class serviceClass, Context context){
@@ -28,5 +42,41 @@ public class Receiver extends BroadcastReceiver {
             }
         }
         return false;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void showNotification(String title, String message, Context context) {
+
+
+        Intent intent = new Intent(context, NotificationMgr.class);
+
+        PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default")
+                .setSmallIcon(R.drawable.mcpieper_icon)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent);
+
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder.setChannelId("com.myApp");
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    "com.myApp",
+                    "My App",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+        notificationManager.notify(2, builder.build());
     }
 }
