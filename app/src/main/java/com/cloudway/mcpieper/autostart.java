03df5ed26705bf976f8378861a1afc0279271e2b
jusax23 <jusax23@gmail.com>
2019-03-09 19:19:03 +0100

    widget/keeper

--- app/src/main/java/com/cloudway/mcpieper/autostart.java
+++ app/src/main/java/com/cloudway/mcpieper/autostart.java
@@ -7,13 +7,10 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
-
-import java.util.Date;
 
 public class autostart extends BroadcastReceiver {
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -24,19 +21,54 @@ public class autostart extends BroadcastReceiver {
                 AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
                 Intent intent_alarm = new Intent(context, Receiver.class);
                 PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 1, intent_alarm, 0);
-                alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+10, pendingIntent);
-                try{
-                    Intent i = new Intent(context, NotificationMgr.class);
-                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    context.startService(i);
-                    Toast.makeText(context, "Autostart", Toast.LENGTH_LONG).show();
-                }catch (Exception e){
-                    Toast.makeText(context, "McPieper halb gestartet!!!\n"+e.toString(), Toast.LENGTH_LONG).show();
+                alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 30000, pendingIntent);
+
+                Intent intent_start = new Intent(context, NotificationMgr.class);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    context.startForegroundService(intent_start);
+                } else {
+                    context.startService(intent_start);
                 }
-            }catch (Exception e){
-                Toast.makeText(context, "Bitte starte Die McPieper app!!!", Toast.LENGTH_LONG).show();
+
+            } catch (Exception e) {
+                showNotification("Error", "Bitte starte die McPieper App!!!", context);
             }
         }
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void showNotification(String title, String message, Context context) {
+
+
+        Intent intent = new Intent(context, MainActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default")
+                .setSmallIcon(R.drawable.mcpieper_icon)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent);
+
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder.setChannelId("com.myApp");
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    "com.myApp",
+                    "My App",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+        notificationManager.notify(2, builder.build());
     }
 
 }
