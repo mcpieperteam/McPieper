614ecbacd77fc4c79c965121bbe3f2e6aabce507
jusax23 <jusax23@gmail.com>
2019-03-16 22:57:50 +0100

    Bud fix

--- app/src/main/java/com/mcpieperteam/mcpieper/NotificationMgr.java
+++ app/src/main/java/com/mcpieperteam/mcpieper/NotificationMgr.java
@@ -161,20 +161,6 @@ public class NotificationMgr extends Service {
 
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            builder.setChannelId("com.myApp");
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(
-                    "com.myApp",
-                    "My App",
-                    NotificationManager.IMPORTANCE_DEFAULT
-            );
-            if (notificationManager != null) {
-                notificationManager.createNotificationChannel(channel);
-            }
-        }*/
         notificationManager.notify(2, builder);
     }
 
