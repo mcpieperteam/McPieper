f931ec9e0d8451e39b1a83949fa4824a8afe7edb
jusax23 <jusax23@gmail.com>
2019-04-06 23:50:31 +0200

    firebase

--- app/src/main/java/com/mcpieperteam/mcpieper/McFirebaseInstanceServiece.java
+++ app/src/main/java/com/mcpieperteam/mcpieper/McFirebaseInstanceServiece.java
@@ -4,6 +4,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -45,12 +46,13 @@ public class McFirebaseInstanceServiece extends FirebaseMessagingService {
                 .setContentText(message)
                 .setDefaults(NotificationCompat.DEFAULT_ALL)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setColor(Color.rgb(205,100,100))
                 .setContentIntent(pendingIntent)
                 .build();
 
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(2, builder);
+        notificationManager.notify("2",2, builder);
     }
 
 }
