0c9ad4a45f675031e6007055f7ad6eaa85534391
jusax23 <jusax23@gmail.com>
2019-04-06 15:16:02 +0200

    firebase

--- app/src/main/java/com/mcpieperteam/mcpieper/McFirebaseInstanceServiece.java
+++ app/src/main/java/com/mcpieperteam/mcpieper/McFirebaseInstanceServiece.java
@@ -1,12 +1,9 @@
 package com.mcpieperteam.mcpieper;
 
-import android.annotation.SuppressLint;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -21,20 +18,24 @@ public class McFirebaseInstanceServiece extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        Log.d("MyFirebase", "recieved");
+        Log.d("MyFirebase", "reseived");
 
-       showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+        Date d = new Date();
+        String day = "morgen";
+        if(d.getHours()<16){
+            day = "heute";
+        }
+        if(!remoteMessage.getData().isEmpty()){
+            showNotification("Du hast "+day+" Dienst", "... mit "+remoteMessage.getData().get("names")+"!!!");
+        }else {
+            showNotification("Du hast "+day+" Dienst", "Vergiss nicht den Pieper abzuholen!!!");
+        }
 
     }
 
-    @SuppressLint("CommitPrefEdits")
     @Override
     public void onNewToken(String s) {
         super.onNewToken(s);
-        SharedPreferences sharedPreferences = getSharedPreferences("login", 0);
-        String old = sharedPreferences.getString("token","");
-        sharedPreferences.edit().putString("token-old",old).commit();
-        sharedPreferences.edit().putString("token",s).commit();
         Log.d("TOKEN", s);
         //sim: di-LqYFW-lI:APA91bETA95DABpWtKkMngcDj8MvidQoFfqt8ihUtAgREQI-bkyIJXEme1Rkw3DXiqbk48RE-BL9XlIijhCNibowNTFF6oe4r_T1g8OgY0aMh_Q90PX69q8J3yYeSwaqxMZvL4yB5izA
         //a7: d9yxWDLgOXY:APA91bG7lbWESDXtvgnOdILZM8yYLqgpIZ6NlyZ_hxprmWot_xisohDVCqlBLrqL_3jX02rKPjMgsAhBUQLH8ZJTM166Vt2UhrD75h9KIsr38qPenwPaRcKIQQvVBn42ZCByreYsNlA9
@@ -53,13 +54,12 @@ public class McFirebaseInstanceServiece extends FirebaseMessagingService {
                 .setContentText(message)
                 .setDefaults(NotificationCompat.DEFAULT_ALL)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setColor(Color.rgb(205,100,100))
                 .setContentIntent(pendingIntent)
                 .build();
 
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify("2",2, builder);
+        notificationManager.notify(2, builder);
     }
 
 }
