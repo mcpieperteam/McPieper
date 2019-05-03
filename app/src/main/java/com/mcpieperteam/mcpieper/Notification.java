9e71c21f2806fccf70c10898fae90beb4f0bbee4
jusax23 <jusax23@gmail.com>
2019-05-03 22:17:17 +0200

    bug fix

--- app/src/main/java/com/mcpieperteam/mcpieper/Notification.java
+++ app/src/main/java/com/mcpieperteam/mcpieper/Notification.java
@@ -168,7 +168,7 @@ public class Notification extends AppCompatActivity implements View.OnClickListe
                                 } else if (result.contains("974")) {
                                     String[] answrs = {"Du hast immernoch Dienst",
                                             "Du hast zu spät abgesagt", "Zu spät... Du hast immernoch Dienst",
-                                            "Du weißt schon, dass du zwischen 16Uhr und 7Uhr absagen musst?"};
+                                            "Du musst zwischen 16Uhr und 7Uhr absagen!!!"};
 
                                     Snackbar.make(view, answrs[new Random().nextInt(answrs.length)], Snackbar.LENGTH_LONG)
                                             .setAction("Action", new View.OnClickListener() {
